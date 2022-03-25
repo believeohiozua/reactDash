b68e63dfa2f4e1a85bbb80cb92b235153fce2dd2
believeohiozua <believe.ohiozua@gmail.com>
2022-03-25 05:10:25 +0100

    update

--- src/components/pages/LandingPage/CaruselSection.js
+++ src/components/pages/LandingPage/CaruselSection.js
@@ -1,22 +1,11 @@
 import React from 'react';
-// src="https://avatars.githubusercontent.com/u/98681?v=4"
-// https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80" height={150} width={100} className="img-responsive d-block w-100
+import moment from 'moment';
+
 const caruselPrevBtn = (id) => {
     let btn = document.getElementById(id);
     btn.click();
 }
-const showLine = (lineid, i) => {
-    let getLine = document.getElementById(lineid);
-    getLine.className = 'd-block';
-    let getDay = document.getElementById(`day-${i + 1}`);
-    getDay.className = 'bg-primary text-white rounded';
-}
-const removeLine = (lineid, i) => {
-    let getLine = document.getElementById(lineid);
-    getLine.className = 'd-none';
-    let getDay = document.getElementById(`day-${i + 1}`);
-    getDay.className = '';
-}
+
 
 const SectionThree = ({ month }) => {
 
@@ -25,7 +14,7 @@ const SectionThree = ({ month }) => {
             <div className="row">
                 <div className="col-3">
                     <p className="fs-bolder">
-                        <b className="">{month} 2021</b>
+                        <b className="">{month.wd} {month.d} {month.m} {month.y}</b>
                         &ensp;  &ensp;
                         <b className="text-muted fa fa-angle-left carousel-controls" onClick={() => caruselPrevBtn('carosel-prev-btn')}></b>
                         &ensp; &ensp;
@@ -50,11 +39,46 @@ const SectionThree = ({ month }) => {
 }
 
 function CaruselSection() {
-
-    var date = new Date().toLocaleDateString("en-US", {
-        "year": "numeric",
-        "month": "numeric"
+    const [dDate, setDdate] = React.useState([]);
+    const [dMonth, setDmonth] = React.useState({
+        d: moment(moment().add(0, 'd').toDate(), 'YYYY/MM/DD').format('ddd'),
+        m: moment(moment().add(0, 'd').toDate(), 'YYYY/MM/DD').format('MMMM'),
+        y: moment(moment().add(0, 'd').toDate(), 'YYYY/MM/DD').format('YYYY')
     });
+
+    const showLine = (lineid, i, item) => {
+        setDmonth({
+            d: item.d,
+            wd: item.wd,
+            m: item.m,
+            y: item.y
+        })
+        let getLine = document.getElementById(lineid);
+        getLine.className = 'd-block';
+        let getDay = document.getElementById(`day-${i + 1}`);
+        getDay.className = 'bg-primary text-white rounded';
+    }
+    const removeLine = (lineid, i, item) => {
+        let getLine = document.getElementById(lineid);
+        getLine.className = 'd-none';
+        let getDay = document.getElementById(`day-${i + 1}`);
+        getDay.className = '';
+    }
+
+    React.useEffect(() => {
+        for (let i = 0; i < 17; i++) {
+            setDdate(prev => [...prev,
+            {
+                day: moment(moment().add(-10 + i, 'd').toDate(), 'YYYY/MM/DD').date(),
+                weekday: moment(moment().add(-10 + i, 'd').toDate(), 'YYYY/MM/DD').format('ddd'),
+                month: moment(moment().add(-10 + i, 'd').toDate(), 'YYYY/MM/DD').format('MMMM'),
+                year: moment(moment().add(-10 + i, 'd').toDate(), 'YYYY/MM/DD').format('YYYY'),
+                counter: -10 + i
+            }
+            ]);
+        }
+    }, [])
+    console.log(dDate);
     return (
         <>
 
@@ -63,7 +87,7 @@ function CaruselSection() {
             <div id="carouselExampleDark" class="carousel carousel-dark slide carousel-fade" data-bs-ride="carousel">
                 <div class="carousel-inner">
                     <div class="carousel-item active" data-bs-interval="10000">
-                        <SectionThree month={"February"} />
+                        <SectionThree month={dMonth} />
                         <div class="row">
                             <div className="col-12" id="set-overlay">
                                 <div className="row">
@@ -112,44 +136,43 @@ function CaruselSection() {
                                 <table class="table table table-borderless">
                                     <thead className='border-0'>
                                         <tr className="text-center">
-                                            <td id="day-1"><span className="text-muted">M</span>1</td>
-                                            <td id="day-2"><span className="text-muted">T</span>2</td>
-                                            <td id="day-3"><span className="text-muted">W</span>3</td>
-                                            <td id="day-4"><span className="text-muted">T</span>4</td>
-                                            <td id="day-5"><span className="text-muted">F</span>5</td>
-                                            <td id="day-6"><span className="text-muted">S</span>T6</td>
-                                            <td id="day-7"><span className="text-muted">S</span>7</td>
-                                            <td id="day-8"><span className="text-muted">M</span>8</td>
-                                            <td id="day-9"><span className="text-muted">T</span>9</td>
-                                            <td id="day-10"><span className="text-muted">W</span>10</td>
-                                            <td id="day-11"><span className="text-muted">T</span>11</td>
-                                            <td id="day-12"><span className="text-muted">F</span>12</td>
-                                            <td id="day-13"><span className="text-muted">S</span>13</td>
-                                            <td id="day-14"><span className="text-muted">S</span>14</td>
-                                            <td id="day-15"><span className="text-muted">M</span>15</td>
-                                            <td id="day-16"><span className="text-muted">T</span>16</td>
-                                            <td id="day-17"><span className="text-muted">W</span>17</td>
+                                            {dDate.map((item, i) => (
+                                                <td
+                                                    id={`day-${i + 1}`}
+                                                    className={item.counter === 0 ? 'bg-primary text-white rounded' : ''}
+                                                    onMouseEnter={() => showLine(`table-item-${i}`, i, { d: item.day, wd: item.weekday, m: item.month, y: item.year })}
+                                                    onMouseLeave={() => removeLine(`table-item-${i}`, i, { d: item.day, wd: item.weekday, m: item.month, y: item.year })}
+
+                                                >
+                                                    <span className="text-muted">
+                                                        {item.weekday.charAt(0)}
+                                                    </span>
+                                                    {item.day}
+                                                </td>
+                                            ))}
                                         </tr>
                                     </thead>
                                     <tbody className="">
                                         <tr>
-                                            {Array.from({ length: 17 }, (_, i) =>
+                                            {dDate.map((item, i) => (
                                                 <>
                                                     {i > 4 ?
                                                         <>
                                                             <td className="border-start border-end text-center vh-100"
-                                                                onMouseEnter={() => showLine(`table-item-${i}`, i)}
-                                                                onMouseLeave={() => removeLine(`table-item-${i}`, i)}
+                                                                onMouseEnter={() => showLine(`table-item-${i}`, i, { d: item.day, wd: item.weekday, m: item.month, y: item.year })}
+                                                                onMouseLeave={() => removeLine(`table-item-${i}`, i, { d: item.day, wd: item.weekday, m: item.month, y: item.year })}
                                                             >
 
-                                                                <div className="d-none" id={`table-item-${i}`}>
+                                                                <div className={item.counter === 0 ? "d-block" : "d-none"} id={`table-item-${i}`}>
                                                                     <i class="fa fa-circle fs-4 text-primary" aria-hidden="true"></i>
                                                                     <p className="vl"></p>
                                                                 </div>
                                                             </td>
                                                         </>
                                                         :
-                                                        <td className=" text-center vh-100" id="table-item">
+                                                        <td className=" text-center vh-100" id="table-item"
+                                                            onMouseEnter={() => showLine(`table-item-${i}`, i, { d: item.day, wd: item.weekday, m: item.month, y: item.year })}
+                                                            onMouseLeave={() => removeLine(`table-item-${i}`, i, { d: item.day, wd: item.weekday, m: item.month, y: item.year })}>
                                                             {i === 7 ?
                                                                 <div className="show-item">
                                                                     <i class="fa fa-circle fs-4 text-primary" aria-hidden="true"></i>
@@ -159,7 +182,7 @@ function CaruselSection() {
                                                         </td>
                                                     }
                                                 </>
-                                            )}
+                                            ))}
                                         </tr>
                                     </tbody>
                                 </table>
